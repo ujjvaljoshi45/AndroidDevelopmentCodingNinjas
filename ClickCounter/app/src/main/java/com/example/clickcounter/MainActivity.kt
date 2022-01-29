507a43327990edd6a0f2bc59e5eadd8f9a895baa
Ujjval <ujjvaljoshi45@gmail.com>
2022-01-29 18:14:05 +0530

    update

--- ClickCounter/app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ ClickCounter/app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.clickcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuItemCompat
 import java.nio.file.Files.size
 import kotlin.random.Random
 
@@ -13,18 +17,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button:Button = findViewById(R.id.button)
-        val imageView:ImageView = findViewById(R.id.imageView)
+//        val button:Button = findViewById(R.id.button)
+//        val imageView:ImageView = findViewById(R.id.imageView)
+//
+//
+//        button.setOnClickListener {
+//            var x = Random.nextInt(0,8)
+//            val color = intArrayOf(R.color.purple_200,R.color.white,R.color.black,R.color.teal_200,R.color.red,R.color.green,R.color.yellow,R.color.purple_700)
+//            val randomColor = color[x]
+//            x += 1
+//            imageView.setImageResource(randomColor)
+//        }
+        val searchView : SearchView = findViewById(R.id.search_view_button)
+        searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("Not yet implemented")
+                return false
+            }
 
-        button.setOnClickListener {
-            var x = Random.nextInt(0,8)
-            val color = intArrayOf(R.color.purple_200,R.color.white,R.color.black,R.color.teal_200,R.color.red,R.color.green,R.color.yellow,R.color.purple_700)
-            val randomColor = color[x]
-
-
-            x += 1
-            imageView.setImageResource(randomColor)
-        }
+        })
     }
 }
